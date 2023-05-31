da1899105a7001732738218c39f171544bdece8e
admin <mmd83@63.ru>
2023-05-31 20:25:10 +0400

    Sprinf frameork

---
+++ first-Beans/src/main/java/ru/SpringCore/first/Beans/FirstBeansApplication.java
@@ -0,0 +1,13 @@
+package ru.SpringCore.first.Beans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstBeansApplication.class, args);
+	}
+
+}
